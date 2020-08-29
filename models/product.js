f8c33fa65c48b7ba781d7400731315fa163d0f3d
raul-ae <raul.ae@hotmail.com>
2020-08-29 09:56:27 -0500

    small update to Products collection

--- models/product.js
+++ models/product.js
@@ -2,20 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
+  sku: { type: String, required: true },
   productName: { type: String, required: true },
   categoryName: { type: String, required: true },
   price: { type: Number, required: true },
   stock: { type: Number, required: true },
   description: { type: String, required: true },
-  sellerId: { 
-    type: Schema.Types.ObjectId, 
-    ref: "seller" 
+  sellerId: {
+    type: Schema.Types.ObjectId,
+    ref: "seller",
   },
-  storeId: { 
-    type: Schema.Types.ObjectId, 
-    ref: "store" 
+  storeId: {
+    type: Schema.Types.ObjectId,
+    ref: "store",
   },
-  picture: { type: String, required: true }
+  picture: { type: String, required: true },
 });
 
 const Product = mongoose.model("Product", productSchema);
