6a17f18d7982371a2191ad949523698ab22d303e
Jorge Guzman <jorguzman100@gmail.com>
2020-09-02 23:06:04 +0300

    StoresConf fornt created

--- client/src/pages/StoresConf.js
+++ client/src/pages/StoresConf.js
@@ -1,17 +1,55 @@
-import React, {useState, useEffect} from "react";
-import Container from 'react-bootstrap/Container';
-import Tab from 'react-bootstrap/Tab'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Nav from 'react-bootstrap/Nav'
+import React from "react";
+import { InputGroup, FormControl, Button, ListGroup } from 'react-bootstrap';
 
+/* 
+  storeName: { type: String, required: true },
+  storePicture: { type: String, required: true },
+  products: [
+    {
+      type:Schema.Types.ObjectId,
+      ref: "product"
+    }
+  ],
+  seller: {
+    type: Schema.Types.ObjectId,
+    ref: "seller"
+  }
+*/
 
-function ProductsConfig() {
+function StoresConfig() {
+    const currentStores = ['Store 1', 'Store 2', 'Store 3']
     return (
         <div>
-            STORES Form to be inserted
+            <h4><strong>Manage your stores</strong></h4>
+
+            <h5>Current stores</h5>
+            <ListGroup>
+                {currentStores.map((store, index) => {
+                    return (
+                        <InputGroup key={index} className="mb-3">
+                            <ListGroup.Item >{store}</ListGroup.Item>
+                            <InputGroup.Append>
+                                <Button variant="outline-secondary">Update</Button>
+                                <Button variant="outline-secondary">Delete</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+                    );
+                })}
+            </ListGroup>
+
+            <h5>Create a new store</h5>
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="Store name"
+                    aria-label="store name"
+                    aria-describedby="basic-addon2"
+                />
+                <InputGroup.Append>
+                    <Button variant="outline-secondary">Create</Button>
+                </InputGroup.Append>
+            </InputGroup>
         </div>
     );
-  }
-  
-  export default ProductsConfig;
+}
+
+export default StoresConfig;
