947a7d8435e93ac2be8f346d14dbc111a35f2d6d
Jorge Guzman <jorguzman100@gmail.com>
2020-09-06 20:43:20 +0300

    Stripe form included in the purchase confirmation form

--- client/src/components/checkoutpayment/index.js
+++ client/src/components/checkoutpayment/index.js
@@ -2,14 +2,22 @@ import React from "react";
 // import "./style.css";
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-//import CheckoutForm from "../CheckoutForm";
+import CheckoutForm from "../CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe("pk_test_51HLY6nIsyYjySygOXa1LA85XDeyEmpELSHi6IGah9ECVTI6zod8Hk5Z7IEFDlNLIjTFLJB5SOTnpI6R5szjarang00wQanVBJ7");
 
 function ProductsList() {
+
+
     return (
         <Card body className="cardRes">
             <Form>
                 Payment Form
-                
+                 <Elements stripe={promise}>
+                    <CheckoutForm />
+                </Elements>
             </Form>
         </Card>
     )
@@ -18,5 +26,34 @@ function ProductsList() {
 export default ProductsList;
 
 
+/*
 
+import React from "react";
+// import "./style.css";
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import CheckoutForm from "../CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe("pk_test_51HLY6nIsyYjySygOXa1LA85XDeyEmpELSHi6IGah9ECVTI6zod8Hk5Z7IEFDlNLIjTFLJB5SOTnpI6R5szjarang00wQanVBJ7");
+
+function ProductsList() {
+
+
+    return (
+        <Card body className="cardRes">
+            <Form>
+                Payment Form
+                 <Elements stripe={promise}>
+                    <CheckoutForm />
+                </Elements>
+            </Form>
+        </Card>
+    )
+}
+
+export default ProductsList;
+
+*/
 
