63cf843f5552aaf849769004b5ec0f15197621d0
Jorge Guzman <jorguzman100@gmail.com>
2020-09-11 00:13:19 +0300

    Dashboard displaying Last Orders Chart

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -203,6 +203,7 @@ const Dashboard = ({ consumers, stores, products, orders }) => {
                     label: "# of Votes",
                     data: ordersPerDateValuesArr,
                     backgroundColor: [
+                        "rgba(75, 192, 192, 0.2)",
                         "rgba(255, 99, 132, 0.2)",
                         "rgba(54, 162, 235, 0.2)",
                         "rgba(255, 206, 86, 0.2)",
@@ -211,6 +212,7 @@ const Dashboard = ({ consumers, stores, products, orders }) => {
                         "rgba(255, 159, 64, 0.2)",
                     ],
                     borderColor: [
+                        "rgba(75, 192, 192, 1)",
                         "rgba(255, 99, 132, 1)",
                         "rgba(54, 162, 235, 1)",
                         "rgba(255, 206, 86, 1)",
@@ -338,12 +340,13 @@ const Dashboard = ({ consumers, stores, products, orders }) => {
                         <Card.Header className="py-1">Last Orders</Card.Header>
                         <Card.Body className="p-1">
                             <div style={{ width: "100%", height: '50%' }}>
-                                <Line
-                                    data={ordersPerDateChartData}
-                                    options={{
-                                        responsive: true,
-                                    }}
-                                />
+                                {ordersPerDateChartData &&
+                                    <Line
+                                        data={ordersPerDateChartData}
+                                        options={{
+                                            responsive: true,
+                                        }}
+                                    />}
                             </div>
                         </Card.Body>
                     </Card>
