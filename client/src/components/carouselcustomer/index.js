0cdc35adc7c7cbf728269fa5806cd89a1a186e76
Rodrigo Rosas <rodrigo.rosasv@gmail.com>
2020-09-10 20:33:55 -0500

    banners

--- client/src/components/carouselcustomer/index.js
+++ client/src/components/carouselcustomer/index.js
@@ -9,7 +9,7 @@ function carousel() {
       <Carousel.Item className="minCar">
         <img
           className="d-block w-100"
-          src="https://themarket.s3.us-east-2.amazonaws.com/Banners/bannerthemarket.jpg"
+          src="https://themarket.s3.us-east-2.amazonaws.com/Banners/banner1.png"
           alt="First slide"
         />
         <Carousel.Caption>
@@ -18,25 +18,21 @@ function carousel() {
       <Carousel.Item className="minCar">
         <img
           className="d-block w-100"
-          src="https://themarket.s3.us-east-2.amazonaws.com/Banners/carouselph2.png"
+          src="https://themarket.s3.us-east-2.amazonaws.com/Banners/banner2.png"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="minCar">
         <img
           className="d-block w-100"
-          src="https://themarket.s3.us-east-2.amazonaws.com/Banners/carouselph2.png"
+          src="https://themarket.s3.us-east-2.amazonaws.com/Banners/bannerthemarket.jpg"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
